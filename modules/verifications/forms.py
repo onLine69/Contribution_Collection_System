062f5b0bfed8edcb1ca59bc2aa0748e700f64ce1
Caine Bautista <115358604+onLine69@users.noreply.github.com>
2024-11-08 02:08:53 +0800

    Final?

--- modules/verifications/forms.py
+++ modules/verifications/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, IntegerField, SelectField, DateTimeLocalField
+from wtforms import StringField, validators, IntegerField, SelectField
 
 
 class VerificationForm(FlaskForm):
     contribution_name = StringField(label="Name", validators=[validators.DataRequired(message="Provide a Name for Identification."), validators.length(min=1,max=50,message="Length not valid. (min=1, max=50)")])
     block_rep = StringField(label="Block Rep", validators=[validators.DataRequired(message="Provide a Name for Identification."), validators.length(min=1,max=50,message="Length not valid. (min=1, max=50)")])
     year_level = SelectField(label="Year Level", choices=[('1', "1st year"), ('2', "2nd year"), ('3', "3rd year"), ('4', "4th year")])
-    program_code = StringField(label="Program Code", validators=[validators.DataRequired(message="Provide the Program Code of the Student."), validators.length(min=1,max=10,message="Length not valid. (min=1, max=10)")])
+    program_code = SelectField(label="Program Code", choices=[])
     total_amount = IntegerField(label="Total Amount (in Php)", validators=[validators.DataRequired(message="Provide the total."), validators.NumberRange(message="Amount not less than 0.", min=0)])
